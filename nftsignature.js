75693557100221b632fe92ebdba2afe92b356e4a
Anatolie Anatoliciva <86365049+Tole1775@users.noreply.github.com>
2026-08-20 12:06:30 -0700

    Fix: convert raw JSON to CommonJS module (nft metadata)

--- nftsignature.js
+++ nftsignature.js
@@ -1,19 +1,21 @@
-{
-  "name": "Asset #9042",
-  "description": "Digital physical design component.",
-  "image": "ipfs://QmYourImageHashHere",
-  "attributes": [
+const metadata = {
+  name: "Asset #9042",
+  description: "Digital physical design component.",
+  image: "ipfs://QmYourImageHashHere",
+  attributes: [
     {
-      "trait_type": "Creator",
-      "value": "Anatolie Anatoliciva"
+      trait_type: "Creator",
+      value: "Anatolie Anatoliciva"
     },
     {
-      "trait_type": "Copyright",
-      "value": "Copyright 2026. All rights reserved."
+      trait_type: "Copyright",
+      value: "Copyright 2026. All rights reserved."
     },
     {
-      "trait_type": "Public_Key",
-      "value": "97271d8e73e5480d95c6c09becbbcf73"
+      trait_type: "Public_Key",
+      value: "97271d8e73e5480d95c6c09becbbcf73"
     }
   ]
-}
+};
+
+module.exports = metadata;
