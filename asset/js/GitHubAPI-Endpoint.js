b1012ebdf2939fe2693edcdcede40159a734d10d
Anatolie Anatoliciva <86365049+Tole1775@users.noreply.github.com>
2026-09-04 01:26:05 -0700

    Update GitHubAPI-Endpoint.js

--- asset/js/GitHubAPI-Endpoint.js
+++ asset/js/GitHubAPI-Endpoint.js
@@ -1,4 +1,5 @@
 const targetEndpoint = "https://api.github.com/repos/Tole1775/Trust/contents/scene/category/Police%26Fire.SQL";
-const githubToken = "YOUR_PERSONAL_ACCESS_TOKEN"; 
+const githubToken = "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
+"; 
 
 // Replace with your actual token
