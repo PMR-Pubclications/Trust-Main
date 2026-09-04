0280360d33b3e0f39294adfab6df5267926da805
Anatolie Anatoliciva <86365049+Tole1775@users.noreply.github.com>
2026-09-04 01:22:52 -0700

    Create GitHubAPI-Endpoint.js

---
+++ asset/js/GitHubAPI-Endpoint.js
@@ -0,0 +1,4 @@
+const targetEndpoint = "https://api.github.com/repos/Tole1775/Trust/contents/scene/category/Police%26Fire.SQL";
+const githubToken = "YOUR_PERSONAL_ACCESS_TOKEN"; 
+
+// Replace with your actual token
