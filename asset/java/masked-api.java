30d0a58dfb588436cea7eed01235fdd86cd8116e
Anatolie Anatoliciva <86365049+Tole1775@users.noreply.github.com>
2026-09-10 23:24:31 -0700

    Create masked-api.java

---
+++ asset/java/masked-api.java
@@ -0,0 +1,5 @@
+public class UrlPrinter {
+    public static void main(String[] args) {
+        System.out.println("https://github.com/Tole1775/Trust/blob/main/asset%2Fjava%2Fkeymask.java");
+    }
+}
