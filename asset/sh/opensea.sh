c40261fcfd85ca304299e67029d045eb8d0bcb14
Anatolie Anatoliciva <86365049+Tole1775@users.noreply.github.com>
2026-09-24 03:05:26 -0700

    Update opensea.sh

--- asset/sh/opensea.sh
+++ asset/sh/opensea.sh
@@ -5,7 +5,7 @@ opensea whoami
 
 export OPENSEA_API_KEY="a098666e9c4c42ca9772f42470bb9014"
 export OPENSEA_PRIVATE_KEY="f25c57a885754235a82996ee8ed9b342"
-export DROP_SLUG="your-drop-slug"
+export DROP_SLUG="ltf"
 
 opensea login --private-key --scopes read:eligibility
 opensea whoami
