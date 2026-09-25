97d1795a818a1f96d8e42a14f96742e57b445576
Anatolie Anatoliciva <86365049+Tole1775@users.noreply.github.com>
2026-09-25 15:21:27 -0700

    Update land.sh

--- sh/land.sh
+++ sh/land.sh
@@ -1,3 +1,5 @@
 docker compose up -d
 docker logs -f lightning-receiver-node
 docker exec -it lightning-receiver-node lncli create
+docker exec -it lightning-receiver-node lncli addinvoice --amt=5000 --memo="Cloud Payout Sweep"
+
